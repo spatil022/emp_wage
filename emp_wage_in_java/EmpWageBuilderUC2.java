1f11195941e7db324633cf399424d01851e917d4
spatil022 <shamalpatil1998@gmail.com>
2021-02-01 21:56:32 +0530

    added emp_wage

---
+++ emp_wage_in_java/EmpWageBuilderUC2.java
@@ -0,0 +1,15 @@
+public class EmpWageBuilderUC2 {
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int emphrs = 0;
+        int empwage = 0;
+        double empcheck = Math.floor(Math.random()*10)%2;
+        if(empcheck == IS_FULL_TIME)
+            emphrs = 8;
+        else
+            emphrs = 0;
+        empwage = emphrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp wage:" + empwage);
+    }
+}
